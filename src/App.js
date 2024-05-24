3b241d9fd55e3673d069bf8ff93098b9d7aa12b4
rupweb <rupert@webstersystems.co.uk>
2024-05-24 15:30:50 +0100

    Align dotmed logo and FX text

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={`${process.env.PUBLIC_URL}/dotmed.jpg`} alt="Dotmed Logo" style={{ width: '100px', height: 'auto' }} />
-        <h1>FX</h1>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <img src={`${process.env.PUBLIC_URL}/dotmed.jpg`} alt="Dotmed Logo" style={{ width: '100px', height: 'auto', marginRight: '10px' }} />
+          <h1>FX</h1>
+        </div>
         <CurrencyConverter />
       </header>
     </div>
