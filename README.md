712c847183377e94be2c6d77a8faa4cd5270163d
rupweb <rupert@webstersystems.co.uk>
2024-05-29 17:57:23 +0100

    Added reverse proxy

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ws-front-end
+
 Webster Systems front end for FIX connection
+
+## frontend
+
+This contains the FX panel JS code. 
+Run it by navigating to frontend directory:
+
+`npm install`
+`npm start`
+
+## backend
+
+This contains an express server.
+Run it by navigating to backend directory:
+
+`npm start`
+
+PS G:\WS\ws-front-end\backend> npm start
+
+> ws-backend@1.0.0 start
+> node server.js
+
+Server running on http://localhost:3001
---
+++ proxy/proxy-server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+// Proxy for the main site
+app.use(
+  '/',
+  createProxyMiddleware({
+    target: 'http://d32psyrastjep.cloudfront.net',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/fxapi': '', // remove /fxapi from the path
+      '^/backend': '', // remove /backend from the path
+    },
+  })
+);
+
+// Proxy for the React app on EC2
+app.use(
+  '/fxapi',
+  createProxyMiddleware({
+    target: 'http://localhost:3000',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/fxapi': '', // remove /fxapi from the path
+    },
+  })
+);
+
+// Proxy for the backend service on EC2
+app.use(
+  '/backend',
+  createProxyMiddleware({
+    target: 'http://localhost:3001',
+    changeOrigin: true,
+  })
+);
+
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => {
+  console.log(`Proxy server is running on port ${PORT}`);
+});
